45d482a1481cc882bbb97a273af53157c01d5bac
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-04-21 19:54:21 +0100

    Initial commit: Angular setup with Tailwind/JSON Server.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"], // Include Angular HTML and TypeScript files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
